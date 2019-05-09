a664a22c92eb2415c8075b8835979a9f99fb48d9
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2019-05-08 19:41:13 -0500

    Pointers pointers everywhere... and if they learn I do not care

--- main.cpp
+++ main.cpp
@@ -4,18 +4,37 @@ using namespace std;
 
 #define DEBUG false
 
+int add_numbers(int &x, int y){
+		// x = x + y;
+		return x+=y;
+}
+
 int main(){
 	
-	bool TRUE = true;
-	
-	if(DEBUG){ cout << "Value of TRUE: " << TRUE << endl; }
 
-	cout << "This is something to the screen." << endl;
-	if(TRUE){
-		cout << "...and more to the screen." << endl;
-	} else {
-		cout << "...else less to screen." << endl;
-	}
+	int num2 = 300;
+	int num1 = 5;
+	int *ptr = &num2;
+	cout << "Value of num2 is " << num2 << endl; 
+	cout << "Value of ptr is " << ptr << endl; 
+	num2 = 9999;
+	cout << "Value of num2 is " << num2 << endl; 
+	cout << "Value of ptr is " << ptr << endl; 
+	ptr = &num1;
+	cout << "Value of num2 is " << num2 << endl; 
+	cout << "Value of ptr is " << ptr << endl; 
+	num1 = 555;
+	cout << "Value of num2 is " << num2 << endl; 
+	cout << "Value of *ptr is " << *ptr << endl; 
+	cout << "Value of ptr is " << ptr << endl; 
+	
+	// cout << "Value of addr is " << addr << endl; 
+	// add_numbers(num1,1);
+	// cout << "Adding 5 to 300: " << num1 << endl;
+	// add_numbers(num1,num2);
+	// cout << "Adding 5 to 300: " << num1 << endl;
+	// add_numbers(num1,num2);
+	// cout << "Adding 5 to 300: " << num1 << endl;
 
 	return 0;	
 }
